714878ab45e0db6f94c0f0d80eeb147edf12c5c8
MARIA EDUARDA TESSARI <maria_tessari@estudante.sesisenai.org.br>
2026-06-24 10:18:01 -0300

    subindo projeto

--- src/server.js
+++ src/server.js
@@ -4,11 +4,14 @@ import { prisma } from "./lib/prisma.ts"
 
 const app = express()
 const PORT = 3000
-app.get("/", async (req, res) =>{
 
-    const user = await prisma.user.findMany()
-    res.json(user)
-})
+app.use(express.json ())
+
+// app.get("/", async (req, res) =>{
+
+//     const user = await prisma.user.findMany()
+//     res.json(user)
+// })
 
 app.listen(PORT, () => {
   console.log("servidor rodando"); // Isso é uma mensagem de log, NÃO um endereço
