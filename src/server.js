cbcba432449fa1601690d2695358db3328314a17
MARIA EDUARDA TESSARI <maria_tessari@estudante.sesisenai.org.br>
2026-06-30 08:53:23 -0300

    atualização de rotas

--- src/server.js
+++ src/server.js
@@ -19,14 +19,29 @@ app.get("/produtos", async (req, res) => {
 
 app.post("/produtos", async(req, res) => {
 const{nome, categoria, quantidade} = req.body
-
-const novoItem = await
-prisma.produtos.create({
-
-  data:{nome, categoria, quantidade: Number(quantidade)}
-})
-
+try{
+const novoItem = await prisma.produtos.create({
+  data: {nome, categoria, quantidade: Number(quantidade)}
 });
+
+res.status(201).json(novoItem)
+}catch(error){
+  res.status(400).json({error: "Erro ao criar produto"})
+}})
+
+app.put("/produtos/:id", async(req, res) => {
+  const {id} = req.params
+  const {nome, categoria, quantidade} = req.body
+
+  const produtoAtualizado = await prisma.produtos.update({
+    where: {id: Number(id)},
+    data: {
+      nome,
+      categoria,
+      quantidade: Number(quantidade)
+    }
+  })
+})
 
 app.listen(PORT, () => {
   console.log("API rodando"); // Isso é uma mensagem de log, NÃO um endereço
